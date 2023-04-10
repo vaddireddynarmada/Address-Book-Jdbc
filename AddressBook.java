81b3a391302d6bf3598a22e53f86d0b53d839dfc
unknown <narmada.vaddireddy97@gmail.com>
2023-04-10 12:09:40 +0530

    Added: Code to add new contacts

--- AddressBook.java
+++ AddressBook.java
@@ -1,16 +1,29 @@
 package org.example;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-
-import java.io.FileWriter;
-import java.io.IOException;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
+    public static final int FIRSTNAME = 1;
+    public static final int LASTNAME = 2;
+    public static final int ADDRESS = 3;
+    public static final int CITY = 4;
+    public static final int STATE = 5;
+    public static final int ZIP = 6;
+    public static final int PHONE_NUMBER = 7;
+    public static final int EMAIL_ID = 8;
+    public static final int EXIT = 9;
+
+    public void createNewTable(Connection connection, String query) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Table already exists");
+        }
+
+    }
+
     public void addContactDataBase(Connection connection) throws SQLException {
         Scanner scanner = new Scanner(System.in);
         String sql = "insert into AddressBook (FirstName,LastName,Address,City,State,Email,ZipCode,PhoneNumber,BookName,BookType) values (?,?,?,?,?,?,?,?,?,?)";
@@ -37,6 +50,7 @@ public class AddressBook {
         preparedStatement.setString(10, scanner.next());
         preparedStatement.executeUpdate();
     }
+
     public void addDetailsContact(Connection connection) throws SQLException {
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("select * from AddressBook");
@@ -56,14 +70,163 @@ public class AddressBook {
             System.out.println(contact);
         }
     }
+
     public void countCity(Connection connection) throws SQLException {
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("select count(*) from AddressBook group by city");
-    while (resultSet.next()){
-        int count = resultSet.getInt(1);
-        System.out.println(count);
+        while (resultSet.next()) {
+            int count = resultSet.getInt(1);
+            System.out.println(count);
+        }
     }
+
+    public void displayBookType(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet1 = statement.executeQuery("SELECT BookType FROM AddressBook GROUP BY BookType");
+        System.out.println("BookType Names :");
+        while (resultSet1.next()) {
+            System.out.print(resultSet1.getString("BookType") + "  ");
+        }
+        System.out.println();
+    }
+
+    public void displayBookContact(Connection connection) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from AddressBook");
+        System.out.println("Enter bookType :");
+        String bookType = scanner.next();
+        while (resultSet.next()) {
+            if (resultSet.getString("BookType").equals(bookType)) {
+                Contacts contact = new Contacts();
+                contact.setID(resultSet.getInt("id"));
+                contact.setFIRST_NAME(resultSet.getString("FirstName"));
+                contact.setLAST_NAME(resultSet.getString("LastName"));
+                contact.setADDRESS(resultSet.getString("Address"));
+                contact.setCITY(resultSet.getString("City"));
+                contact.setSTATE(resultSet.getString("State"));
+                contact.setZip(resultSet.getInt("ZipCode"));
+                contact.setEMAIL_ID(resultSet.getString("Email"));
+                contact.setPHONE_NUMBER(resultSet.getDouble("PhoneNumber"));
+                contact.setBOOK_NAME(resultSet.getString("BookName"));
+                contact.setBOOK_TYPE(resultSet.getString("BookType"));
+                System.out.println(contact);
+            }
+        }
+    }
+
+    public void deleteContact(Connection connection) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter firstname to delete ");
+        String firstname = scanner.next();
+        PreparedStatement preparedStatement = connection.prepareStatement("delete from AddressBook where firstName=?");
+        preparedStatement.setString(1, firstname);
+        preparedStatement.executeUpdate();
+    }
+
+    public void editContact(Connection connection) throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        PreparedStatement preparedStatement;
+        System.out.println("Enter bookType to Edit :  ");
+        String bookType = sc.next();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from AddressBook");
+        boolean condition = true;
+        while (condition) {
+            while (resultSet.next()) {
+                if (resultSet.getString("BookType").equals(bookType)) {
+                    System.out.println("enter which details wants to edit in contact");
+                    System.out.println("1.firstName, 2.lastName, 3.address, 4.city, 5.state, 6.zip,7.phoneNumber, 8.emailId, 9.exit");
+                    int editDetails = sc.nextInt();
+                    switch (editDetails) {
+                        case FIRSTNAME:
+                            preparedStatement = connection.prepareStatement("update AddressBook set FirstName=? where id=?");
+                            System.out.println("Enter Id");
+                            String firstnameId = sc.next();
+                            System.out.println("Enter firstname");
+                            String firstname = sc.next();
+                            preparedStatement.setString(1, firstname);
+                            preparedStatement.setString(2, firstnameId);
+                            preparedStatement.executeUpdate();
+                            break;
+                        case LASTNAME:
+                            preparedStatement = connection.prepareStatement("update AddressBook set LastName=? where id=?");
+                            System.out.println("Enter Existing FirstName");
+                            int lastNameId = sc.nextInt();
+                            System.out.println("Enter LastName");
+                            String LastName = sc.next();
+                            preparedStatement.setString(1, LastName);
+                            preparedStatement.setInt(2, lastNameId);
+                            preparedStatement.executeUpdate();
+                            break;
+                        case ADDRESS:
+                            preparedStatement = connection.prepareStatement("update AddressBook set Address=? where id=?");
+                            System.out.println("Enter Existing FirstName");
+                            String addressId = sc.next();
+                            System.out.println("Enter Address");
+                            String Address = sc.next();
+                            preparedStatement.setString(1, Address);
+                            preparedStatement.setString(3, addressId);
+                            preparedStatement.executeUpdate();
+                            break;
+                        case CITY:
+                            preparedStatement = connection.prepareStatement("update AddressBook set City=?where id=?");
+                            System.out.println("Enter Existing FirstName");
+                            String cityId = sc.next();
+                            System.out.println("Enter City");
+                            String City = sc.next();
+                            preparedStatement.setString(1, City);
+                            preparedStatement.setString(2, cityId);
+                            preparedStatement.executeUpdate();
+                            break;
+                        case STATE:
+                            preparedStatement = connection.prepareStatement("update AddressBook set State=? where id=?");
+                            System.out.println("Enter Existing FirstName");
+                            int stateId = sc.nextInt();
+                            System.out.println("Enter State");
+                            String State = sc.next();
+                            preparedStatement.setString(1, State);
+                            preparedStatement.setInt(2, stateId);
+                            preparedStatement.executeUpdate();
+                            break;
+                        case EMAIL_ID:
+                            preparedStatement = connection.prepareStatement("update AddressBook set Email=?where id=?");
+                            System.out.println("Enter Id to edit");
+                            int emailId = sc.nextInt();
+                            System.out.println("Enter Email");
+                            String Email = sc.next();
+                            preparedStatement.setString(1, Email);
+                            preparedStatement.setInt(2, emailId);
+                            preparedStatement.executeUpdate();
+                            break;
+                        case ZIP:
+                            preparedStatement = connection.prepareStatement("update AddressBook set ZipCode=?where id=?");
+                            System.out.println("Enter Id to edit");
+                            int zipId = sc.nextInt();
+                            System.out.println("Enter ZipCode");
+                            int ZipCode = sc.nextInt();
+                            preparedStatement.setInt(1, ZipCode);
+                            preparedStatement.setInt(2, zipId);
+                            preparedStatement.executeUpdate();
+                            break;
+                        case PHONE_NUMBER:
+                            preparedStatement = connection.prepareStatement("update AddressBookSystem set PhoneNumber=?where id=?");
+                            System.out.println("Enter Id to edit");
+                            int phoneNumberId = sc.nextInt();
+                            System.out.println("Enter phoneNumber");
+                            double PhoneNumber = sc.nextDouble();
+                            preparedStatement.setDouble(1, PhoneNumber);
+                            preparedStatement.setDouble(2, phoneNumberId);
+                            preparedStatement.executeUpdate();
+                            break;
+                        case EXIT:
+                            condition = false;
+                            break;
+                    }
+                    break;
+                }
+            }
+        }
     }
 }
-
 
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -5,9 +5,15 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class AddressBookMain {
-    private static final int ADD_DETAILS_DATABASE = 1;
-    private static final int ADD_DETAILS_CONTACT = 2;
-    private static final int COUNT_BY_CITY = 3;
+    private static final int CREATE_TABLE_ADDRESS_BOOK = 1;
+    private static final int ADD_DETAILS_DATABASE = 2;
+    private static final int ADD_DETAILS_CONTACT = 3;
+    private static final int COUNT_BY_CITY = 4;
+    private static final int DISPLAY_ADDRESSBOOK_TYPE =5 ;
+    private static final int DISPLAY_CONTACTS_BOOKTYPE = 6;
+    private static final int DELETE = 7;
+    private static final int EDIT_BOOK_CONTACT = 8;
+    private static final int ADD_NEW_CONTACT = 9;
 
     public static void main(String[] args) throws SQLException {
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/AddressBookSystem", "root", "narmada02");
@@ -16,9 +22,26 @@ public class AddressBookMain {
         boolean condition = true;
         while (condition) {
             System.out.println("Enter what you want to perform");
-            System.out.println("1.AddContactDataBase, 2.AddDetailsContact, 3.CountCity");
+            System.out.println("1.CreateTable, 2.AddContactDataBase, 3.AddDetailsContact, 4.CountCity, 5.DisplayBookType, 6.DisplayBookContact, 7.DeleteContact" +
+                    "8.EditBookContact");
            int choice=scanner.nextInt();
            switch (choice){
+               case CREATE_TABLE_ADDRESS_BOOK:
+                    String query = "create table AddressBook( " +
+                            "id INT unsigned not null auto_increment," +
+                            "FirstName varchar(20) not null ," +
+                            "LastName varchar(20) not null , " +
+                            "Address varchar(100) not null , " +
+                            "City varchar(30) not null ," +
+                            "State varchar(30) not null , " +
+                            "Email varchar(30) not null , " +
+                            "ZipCode int not null ," +
+                            "PhoneNumber int not null," +
+                            "BookName varchar(25) not null," +
+                            "BookType varchar(20) not null, " +
+                            "primary key (id))";
+                    addressBook.createNewTable(connection, query);
+                    break;
                case ADD_DETAILS_DATABASE:
                    addressBook.addContactDataBase(connection);
                    break;
@@ -27,6 +50,23 @@ public class AddressBookMain {
                    break;
                case COUNT_BY_CITY:
                    addressBook.countCity(connection);
+                   break;
+               case DISPLAY_ADDRESSBOOK_TYPE:
+                   addressBook.displayBookType(connection);
+                   break;
+               case DISPLAY_CONTACTS_BOOKTYPE:
+                   addressBook.displayBookType(connection);
+                   addressBook.displayBookContact(connection);
+                   break;
+               case EDIT_BOOK_CONTACT:
+                   addressBook.addDetailsContact(connection);
+                   addressBook.displayBookType(connection);
+                   addressBook.editContact(connection);
+                   break;
+               case DELETE:
+                   addressBook.addDetailsContact(connection);
+                   addressBook.displayBookType(connection);
+                   addressBook.deleteContact(connection);
                    break;
            }
         }
